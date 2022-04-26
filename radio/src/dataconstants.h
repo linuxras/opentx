8279ccfc9a1f62331986a796de5e30664705d669
janek <janek.janek@gmail.com>
2022-04-26 23:18:59 +0200

    Enable 16 channels trainer

--- radio/src/dataconstants.h
+++ radio/src/dataconstants.h
@@ -77,7 +77,7 @@
   #define MAX_EXPOS                    14
   #define MAX_LOGICAL_SWITCHES         12
   #define MAX_SPECIAL_FUNCTIONS        11 // number of functions assigned to switches
-  #define MAX_TRAINER_CHANNELS         8
+  #define MAX_TRAINER_CHANNELS         16
   #define MAX_INPUTS                   16
   #define MAX_TELEMETRY_SENSORS        26 // 48b each
   #define MAX_SCRIPTS				           0
