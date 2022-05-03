b2b0816bf412fe09accad67cd0218a604863e101
janek <janek.janek@gmail.com>
2022-05-03 19:17:02 +0200

    Reduce watchdogSuspend to 2s

--- radio/src/gui/128x64/view_main.cpp
+++ radio/src/gui/128x64/view_main.cpp
@@ -294,7 +294,7 @@ void onMainViewMenu(const char *result)
     chainMenu(menuStatisticsView);
   }
   else if (result == STR_SAVEALLDATA) {
-    watchdogSuspend(400 /*4s*/);
+    watchdogSuspend(200 /*2s*/);
     saveAllData();
   }
 #if !defined(PCBI6X)
